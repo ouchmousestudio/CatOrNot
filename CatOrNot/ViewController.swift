725385d6ce1f78251f0508db6372bac15bc8d919
Miles <mairuzu@gmail.com>
2019-08-31 11:39:04 +0900

    Initial Commit

---
+++ CatOrNot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatOrNot
+//
+//  Created by Miles Fearnall-Williams on 2019/08/31.
+//  Copyright © 2019 Miles Fearnall-Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
